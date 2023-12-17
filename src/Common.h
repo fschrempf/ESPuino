6fe9ea8271b89778f20d7a9a41396735d9e2e30d
Frieder Schrempf <frieder@fris.de>
2023-12-17 20:42:23 +0100

    Common.h: Add an IS_ENABLED() helper macro
    The IS_ENABLED() macro allows to evaluate a configuration macro's value
    and can be used by the preprocessor or the compiler.
    
    This is derived (or rather copied) from the Zephyr project codebase.
    Licenses between the source (Apache 2.0) and the target codebase (GPLv3)
    are compatible.

--- src/Common.h
+++ src/Common.h
@@ -1,5 +1,48 @@
 #pragma once
 
+/*
+ * IS_ENABLED() helpers
+ *
+ * Derived from include/zephyr/sys/util_internal.h
+ *
+ * Copyright (c) 2011-2014, Wind River Systems, Inc.
+ * Copyright (c) 2020, Nordic Semiconductor ASA
+ */
+
+#define IS_ENABLED(macro) _IS_ENABLED1(macro)
+
+/* This is called from IS_ENABLED(), and sticks on a "_XXXX" prefix,
+ * it will now be "_XXXX1" if config_macro is "1", or just "_XXXX" if it's
+ * undefined.
+ *   ENABLED:   Z_IS_ENABLED2(_XXXX1)
+ *   DISABLED   Z_IS_ENABLED2(_XXXX)
+ */
+#define _IS_ENABLED1(config_macro) _IS_ENABLED2(_XXXX##config_macro)
+
+/* Here's the core trick, we map "_XXXX1" to "_YYYY," (i.e. a string
+ * with a trailing comma), so it has the effect of making this a
+ * two-argument tuple to the preprocessor only in the case where the
+ * value is defined to "1"
+ *   ENABLED:    _YYYY,    <--- note comma!
+ *   DISABLED:   _XXXX
+ */
+#define _XXXX1 _YYYY,
+
+/* Then we append an extra argument to fool the gcc preprocessor into
+ * accepting it as a varargs macro.
+ *                         arg1   arg2  arg3
+ *   ENABLED:   _IS_ENABLED3(_YYYY,    1,    0)
+ *   DISABLED   _IS_ENABLED3(_XXXX 1,  0)
+ */
+#define _IS_ENABLED2(one_or_two_args) _IS_ENABLED3(one_or_two_args 1, 0)
+
+/* And our second argument is thus now cooked to be 1 in the case
+ * where the value is defined to 1, and 0 if not:
+ */
+#define _IS_ENABLED3(ignore_this, val, ...) val
+
+/* End of IS_ENABLED() helpers */
+
 // FilePathLength
 #define MAX_FILEPATH_LENTGH 256
 
